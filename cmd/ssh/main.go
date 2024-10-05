35afe6e828029af8bb45b1cfb4fedc991c3d529a
Cristiano Lemes <cslemess@gmail.com>
2024-10-05 14:47:34 -0300

    disable standard ssh

--- cmd/ssh/main.go
+++ cmd/ssh/main.go
@@ -17,7 +17,6 @@ import (
 	bm "github.com/charmbracelet/wish/bubbletea"
 	"github.com/charmbracelet/wish/logging"
 	resume "github.com/cslemes/cris_term/cmd/app"
-	_ "github.com/joho/godotenv"
 )
 
 const (
@@ -25,38 +24,33 @@ const (
 	port = 42069
 )
 
-// You can wire any Bubble Tea model up to the middleware with a function that
-// handles the incoming ssh.Session. Here we just grab the terminal info and
-// pass it to the new model. You can also return tea.ProgramOptions (such as
-// tea.WithAltScreen) on a session by session basis.
 func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
-	pty, _, active := s.Pty()
-	if !active {
-		wish.Fatalln(s, "no active terminal, skipping")
-		return nil, nil
-	}
+	// pty, _, active := s.Pty()
+	// if !active {
+	// 	wish.Fatalln(s, "no active terminal, skipping")
+	// 	return nil, nil
+	// }
 	m := resume.InitialModel()
 
 	return m, []tea.ProgramOption{
 		tea.WithAltScreen(),
 
-		tea.WithInput(pty.Slave),
-		tea.WithOutput(pty.Slave),
+		//		tea.WithInput(pty.Slave),
+		//		tea.WithOutput(pty.Slave),
 	}
 }
 
 func main() {
 
 	s, err := wish.NewServer(
-		// wish.WithAddress(ln.Addr().String()),
 		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
 		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
 
-		wish.WithPublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
-			log.Info("Attempting to authenticate with public key", "user", ctx.User(), "key", key.Type())
-			// Accept any public key, regardless of its content.
-			return true
-		}),
+		// wish.WithPublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
+		// 	log.Info("Attempting to authenticate with public key", "user", ctx.User(), "key", key.Type())
+		// 	// Accept any public key, regardless of its content.
+		// 	return true
+		// }),
 		wish.WithMiddleware(
 			bm.Middleware(teaHandler),
 			activeterm.Middleware(),
@@ -69,9 +63,8 @@ func main() {
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	log.Info("Starting SSH server", s.Addr)
 
-	//log.Info("Starting SSH server", "host", host, "port", port)
+	log.Info("Starting SSH server", "host", host, "port", port)
 	go func() {
 		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
 			log.Error("could not start server", "error", err)
